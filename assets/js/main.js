7f1333873c5ef6a11ab0c79f21901d1e4812bd4b
Mirek Behina <mirekbehina@seznam.cz>
2020-01-18 20:24:00 +0100

    scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,15 @@ $(function () {
 		$body.removeClass('is-loading');
 	});
 
+	window.onscroll = function () { myFunction() };
+
+	function myFunction() {
+		var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+		var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+		var scrolled = (winScroll / height) * 100;
+		document.getElementById("myBar").style.width = scrolled + "%";
+	}
+
 
 	particlesJS(
 		"particles-js", {
