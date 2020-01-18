f8876f2b311b023fe919ae1b7b2fd40e381f4148
Mirek Behina <mirekbehina@seznam.cz>
2020-01-18 16:22:14 +0100

    formating
    formating

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,57 +4,70 @@
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 
-$(function() {
+$(function () {
 
 	// Vars.
-		var	$window = $(window),
-			$body = $('body'),
-			$wrapper = $('#wrapper');
+	var $window = $(window),
+		$body = $('body'),
+		$wrapper = $('#wrapper');
 
 	// Breakpoints.
-		skel.breakpoints({
-			xlarge:	'(max-width: 1680px)',
-			large:	'(max-width: 1280px)',
-			medium:	'(max-width: 980px)',
-			small:	'(max-width: 736px)',
-			xsmall:	'(max-width: 480px)'
-		});
+	skel.breakpoints({
+		xlarge: '(max-width: 1680px)',
+		large: '(max-width: 1280px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)'
+	});
 
 	// Disable animations/transitions until everything's loaded.
-		$body.addClass('is-loading');
+	$body.addClass('is-loading');
 
-		$window.on('load', function() {
-			$body.removeClass('is-loading');
-		});
+	$window.on('load', function () {
+		$body.removeClass('is-loading');
+	});
 
 
-		particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},
-"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},
-"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,
-"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1}
-,"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,
-"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":
-{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},
-"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},
-"remove":{"particles_nb":2}}},"retina_detect":true});
+	particlesJS(
+		"particles-js", {
+			"particles": {
+				"number": { "value": 80, "density": { "enable": true, "value_area": 800 } }, "color": { "value": "#ffffff" },
+				"shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 5 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 } },
+				"opacity": { "value": 0.5, "random": false, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false } }, "size": {
+					"value": 3, "random": true,
+					"anim": { "enable": false, "speed": 40, "size_min": 0.1, "sync": false }
+				}, "line_linked": { "enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 }
+				, "move": {
+					"enable": true, "speed": 6, "direction": "none", "random": false, "straight": false, "out_mode": "out", "bounce": false,
+					"attract": { "enable": false, "rotateX": 600, "rotateY": 1200 }
+				}
+			}, "interactivity": {
+				"detect_on": "canvas", "events":
+					{ "onhover": { "enable": true, "mode": "repulse" }, "onclick": { "enable": true, "mode": "push" }, "resize": true }, "modes": {
+						"grab": { "distance": 400, "line_linked": { "opacity": 1 } },
+						"bubble": { "distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3 }, "repulse": { "distance": 200, "duration": 0.4 }, "push": { "particles_nb": 4 },
+						"remove": { "particles_nb": 2 }
+					}
+			}, "retina_detect": true
+	});
 	// Poptrox.
-		$window.on('load', function() {
+	$window.on('load', function () {
 
-			$('.thumbnails').poptrox({
-				onPopupClose: function() { $body.removeClass('is-covered'); },
-				onPopupOpen: function() { $body.addClass('is-covered'); },
-				baseZIndex: 10001,
-				useBodyOverflow: false,
-				usePopupEasyClose: true,
-				overlayColor: '#000000',
-				overlayOpacity: 0.75,
-				popupLoaderText: '',
-				fadeSpeed: 500,
-				usePopupDefaultStyling: false,
-				windowMargin: (skel.breakpoint('small').active ? 5 : 50)
-			});
-
+		$('.thumbnails').poptrox({
+			onPopupClose: function () { $body.removeClass('is-covered'); },
+			onPopupOpen: function () { $body.addClass('is-covered'); },
+			baseZIndex: 10001,
+			useBodyOverflow: false,
+			usePopupEasyClose: true,
+			overlayColor: '#000000',
+			overlayOpacity: 0.75,
+			popupLoaderText: '',
+			fadeSpeed: 500,
+			usePopupDefaultStyling: false,
+			windowMargin: (skel.breakpoint('small').active ? 5 : 50)
 		});
+
+	});
 
 });
 
