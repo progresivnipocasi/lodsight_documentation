5322f5db0af5e7d33f8f8b53b9a8032bf1a496cb
Mirek Behina <mirekbehina@seznam.cz>
2020-01-18 20:26:06 +0100

    removed scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,12 +25,13 @@ $(function () {
 
 	window.onscroll = function () { myFunction() };
 
+	/*
 	function myFunction() {
 		var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
 		var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 		var scrolled = (winScroll / height) * 100;
 		document.getElementById("myBar").style.width = scrolled + "%";
-	}
+	}*/
 
 
 	particlesJS(
